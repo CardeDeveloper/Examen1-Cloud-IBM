80c8ecd209fe51f92cd38d3c303b095afa018c4d
Oscar Angel Cardenas Aguilar <oscar.cardenas@oscar.cardenas>
2021-03-19 06:07:25 -0600

    add tone analyzer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Examen CLOUD' });
 });
+
+router.get('/autor', function(req, res, next) {
+  res.json({ alumno: 'OACA' , servicio:"Cloud Foundry en IBM Cloud"});
+});
+
+router.post('/tone', function(req, res, next) {
+  res.json({ alumno: 'OACA' , servicio:"Cloud Foundry en IBM Cloud"});
+});
+
 
 module.exports = router;
