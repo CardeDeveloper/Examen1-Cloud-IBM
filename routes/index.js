b74aa89ced983cf40e7a0593bc04c6bf8948169b
Oscar Angel Cardenas Aguilar <oscar.cardenas@oscar.cardenas>
2021-03-19 06:20:05 -0600

    Adding tone path

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +13,36 @@ router.get('/autor', function(req, res, next) {
 });
 
 router.post('/tone', function(req, res, next) {
-  res.json({ alumno: 'OACA' , servicio:"Cloud Foundry en IBM Cloud"});
+  if(req.body.msg){
+  const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21' ,
+    authenticator: new IamAuthenticator({
+      apikey: process.env.APIKEY_TONE,
+    }),
+    serviceUrl: process.env.URL_TONE,
+  });
+  const text = req.body.msg
+
+  const toneParams = {
+    toneInput: { 'text': text },
+    contentType: 'application/json',
+  };
+
+  toneAnalyzer.tone(toneParams)
+    .then(toneAnalysis => {
+      res.status(200).json(toneAnalysis.result);
+      console.log(JSON.stringify(toneAnalysis, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+      res.status(500).json({ error: err, servicio:"tone analyzer"});
+      
+    });
+  }else{
+    res.status(400).json({ error: 'Debes enviar msg en el body de la peticion' , servicio:"tone analyzer"});
+  }
+  
+
 });
 
 
